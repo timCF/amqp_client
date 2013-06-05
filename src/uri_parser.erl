5e83ac2c3b922789ff4c4b5f4570471266f78cec
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2013-06-05 16:10:41 +0200

    Update to 3.1.1

--- src/uri_parser.erl
+++ src/uri_parser.erl
@@ -41,9 +41,9 @@
 
 parse(AbsURI, Defaults) ->
     case parse_scheme(AbsURI) of
-    {error, Reason} ->
-        {error, Reason};
-    {Scheme, Rest} ->
+	{error, Reason} ->
+	    {error, Reason};
+	{Scheme, Rest} ->
             case (catch parse_uri_rest(Rest, true)) of
                 [_|_] = List ->
                     merge_keylists([{scheme, Scheme} | List], Defaults);
@@ -61,7 +61,7 @@ parse_scheme(AbsURI) ->
 parse_uri_rest("//" ++ URIPart, true) ->
     %% we have an authority
     {Authority, PathQueryFrag} =
-    split_uri(URIPart, "/|\\?|#", {URIPart, ""}, 1, 0),
+	split_uri(URIPart, "/|\\?|#", {URIPart, ""}, 1, 0),
     AuthorityParts = parse_authority(Authority),
     parse_uri_rest(PathQueryFrag, false) ++ AuthorityParts;
 parse_uri_rest(PathQueryFrag, _Bool) ->
@@ -107,11 +107,11 @@ split_uri(UriPart, SplitChar, NoMatchResult) ->
 
 split_uri(UriPart, SplitChar, NoMatchResult, SkipLeft, SkipRight) ->
     case re:run(UriPart, SplitChar) of
-    {match, [{Match, _}]} ->
-        {string:substr(UriPart, 1, Match + 1 - SkipLeft),
-         string:substr(UriPart, Match + 1 + SkipRight, length(UriPart))};
-    nomatch ->
-        NoMatchResult
+	{match, [{Match, _}]} ->
+	    {string:substr(UriPart, 1, Match + 1 - SkipLeft),
+	     string:substr(UriPart, Match + 1 + SkipRight, length(UriPart))};
+	nomatch ->
+	    NoMatchResult
     end.
 
 merge_keylists(A, B) ->
